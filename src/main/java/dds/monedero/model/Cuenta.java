b605676c48024f76a183cc4588110fb3aebd20e6
nico <nicolasnk16@gmail.com>
2022-05-12 17:16:49 -0300

    Code Smell - Long Parameter List ( el metodo agregarMovimiento de la clase Cuenta facilmente podria recibir un objeto  "movimiento", no tiene la necesidad de conocer todos los atributos de un movimiento y crearlo al momento de agregar.

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -54,8 +54,7 @@ public class Cuenta {
     new Movimiento(LocalDate.now(), cuanto, false).agregateA(this);
   }
 
-  public void agregarMovimiento(LocalDate fecha, Double cuanto, boolean esDeposito) {
-    Movimiento movimiento = new Movimiento(fecha, cuanto, esDeposito);
+  public void agregarMovimiento(Movimiento movimiento) {
     movimientos.add(movimiento);
   }
 
